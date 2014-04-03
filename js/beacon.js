bc70a02c00039721d45db814c297566dc5bd40cf
Brandon Dement <bdement@brightroll.com>
2014-04-03 13:11:46 -0700

    code review feedback changes

--- js/beacon.js
+++ js/beacon.js
@@ -1,139 +1,124 @@
-var OpenVV_OVVID = (function(){
+window.OpenVV_OVVID = (function() {
 
-	var id = 'OVVID'; // 'OVVID' is string substituted from AS
-	var beaconsStarted = {};
+    // 'OVVID' is string substituted from AS
+    var id = 'OVVID';
+    var beaconsStarted = {};
 
-	function findPlayer()
-	{
-		var embeds = document.getElementsByTagName("embed");
+    function findPlayer() {
+        var embeds = document.getElementsByTagName('embed');
 
-		for (var i = 0; i < embeds.length; i++)
-		{
-			if (!!embeds[i][id])
-			{
-				return embeds[i];
-			}
-		}
+        for (var i = 0; i < embeds.length; i++) {
+            if (embeds[i][id]) {
+                return embeds[i];
+            }
+        }
 
-		var objs = document.getElementsByTagName("object");
+        var objs = document.getElementsByTagName('object');
 
-		for (var i = 0; i < objs.length; i++)
-		{
-			if (!!objs[i][id])
-			{
-				return objs[i];
-			}
-		}
+        for (var i = 0; i < objs.length; i++) {
+            if (objs[i][id]) {
+                return objs[i];
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	function addSWFs(url)
-	{
-		if(url === "" || url === ("BEACON" + "_SWF_" + "URL"))
-		{
-			return;
-		}
+    function addSWFs(url) {
+        if (url === '' || url === ('BEACON' + '_SWF_' + 'URL')) {
+            return;
+        }
 
-		var player = findPlayer();
+        var player = findPlayer();
 
-		if (player === null)
-		{
-			console.error("Couldn't find player!");
-			return;
-		}
+        if (player === null) {
+            return;
+        }
 
-		var playerLocation = player.getClientRects()[0];
-		var wmode = "transparent"; //TODO
-		var BEACON_SIZE = 20; //TODO
+        var playerLocation = player.getClientRects()[0];
+        var BEACON_SIZE = 20; //TODO
 
-		for(var index = 1; index <= 5; index++)
-		{
-			var left, top;
+        for (var index = 1; index <= 5; index++) {
+            var left, top;
 
-			switch(index)
-			{
-				case 1: // TOP LEFT
-					left = playerLocation.left;
-					top = playerLocation.top;
-					break;
+            switch (index) {
+                case 1: // TOP LEFT
+                    left = playerLocation.left;
+                    top = playerLocation.top;
+                    break;
 
-				case 2: // TOP RIGHT
-					left = playerLocation.left + playerLocation.width - BEACON_SIZE;
-					top = playerLocation.top;
-					break;
+                case 2: // TOP RIGHT
+                    left = playerLocation.left + playerLocation.width - BEACON_SIZE;
+                    top = playerLocation.top;
+                    break;
 
-				case 3: // CENTER
-					left = playerLocation.left + ((playerLocation.width - BEACON_SIZE) / 2);
-					top = playerLocation.top + ((playerLocation.height - BEACON_SIZE) / 2);
-					break;
+                case 3: // CENTER
+                    left = playerLocation.left + ((playerLocation.width - BEACON_SIZE) / 2);
+                    top = playerLocation.top + ((playerLocation.height - BEACON_SIZE) / 2);
+                    break;
 
-				case 4: // BOTTOM LEFT
-					left = playerLocation.left;
-					top = playerLocation.top + playerLocation.height - BEACON_SIZE;
-					break;
+                case 4: // BOTTOM LEFT
+                    left = playerLocation.left;
+                    top = playerLocation.top + playerLocation.height - BEACON_SIZE;
+                    break;
 
-				case 5: // BOTTOM RIGHT
-					left = playerLocation.left + playerLocation.width - BEACON_SIZE;
-					top = playerLocation.top + playerLocation.height - BEACON_SIZE;
-					break;
-			}
+                case 5: // BOTTOM RIGHT
+                    left = playerLocation.left + playerLocation.width - BEACON_SIZE;
+                    top = playerLocation.top + playerLocation.height - BEACON_SIZE;
+                    break;
+            }
 
-			var swfContainer = document.createElement("DIV");
-				swfContainer.id = "OVVBeaconContainer_" + index + "_" + id;
-				swfContainer.style.position = "absolute";
-				swfContainer.style.zIndex = 99999;
-				swfContainer.style.left = left + "px";
-				swfContainer.style.top = top + "px";
+            var swfContainer = document.createElement('DIV');
+            swfContainer.id = 'OVVBeaconContainer_' + index + '_' + id;
+            swfContainer.style.position = 'absolute';
+            swfContainer.style.zIndex = 99999;
+            swfContainer.style.left = left + 'px';
+            swfContainer.style.top = top + 'px';
 
-			var html =
-				'<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="' + BEACON_SIZE + '" height="' + BEACON_SIZE + '">' +
-					'<param name="movie" value="' + url + '?id=' + id + '&index=' + index +'" />' +
-					'<param name="quality" value="low" />' +
-					'<param name="bgcolor" value="#ffffff" />' +
-					'<param name="wmode" value="' + wmode + '" />' +
-					'<param name="allowScriptAccess" value="always" />' +
-					'<param name="allowFullScreen" value="false" />' +
-						'<!--[if !IE]>-->' +
-						'<object id="OVVBeacon_' + index + '_' + id + '" type="application/x-shockwave-flash" data="' + url + '?id=' + id + '&index=' + index +'" width="' + BEACON_SIZE + '" height="' + BEACON_SIZE + '">' +
-							'<param name="quality" value="low" />' +
-							'<param name="bgcolor" value="#ff0000" />' +
-							'<param name="wmode" value="' + wmode + '" />' +
-							'<param name="allowScriptAccess" value="always" />' +
-							'<param name="allowFullScreen" value="false" />' +
-						'<!--<![endif]-->' +
-				'</object>';
+            var html =
+                '<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="' + BEACON_SIZE + '" height="' + BEACON_SIZE + '">' +
+                '<param name="movie" value="' + url + '?id=' + id + '&index=' + index + '" />' +
+                '<param name="quality" value="low" />' +
+                '<param name="bgcolor" value="#ffffff" />' +
+                '<param name="wmode" value="transparent" />' +
+                '<param name="allowScriptAccess" value="always" />' +
+                '<param name="allowFullScreen" value="false" />' +
+                '<!--[if !IE]>-->' +
+                '<object id="OVVBeacon_' + index + '_' + id + '" type="application/x-shockwave-flash" data="' + url + '?id=' + id + '&index=' + index + '" width="' + BEACON_SIZE + '" height="' + BEACON_SIZE + '">' +
+                '<param name="quality" value="low" />' +
+                '<param name="bgcolor" value="#ff0000" />' +
+                '<param name="wmode" value="transparent" />' +
+                '<param name="allowScriptAccess" value="always" />' +
+                '<param name="allowFullScreen" value="false" />' +
+                '<!--<![endif]-->' +
+                '</object>';
 
-			swfContainer.innerHTML = html;
-			document.body.insertBefore(swfContainer, document.body.firstChild);
-		}
-	}
+            swfContainer.innerHTML = html;
+            document.body.insertBefore(swfContainer, document.body.firstChild);
+        }
+    }
 
-	function isPlayerVisible()
-	{
-		var visible = 0;
+    function isPlayerVisible() {
+        var visible = 0;
 
-		for(var index = 1; index <= 5; index++)
-		{
-			if(document.getElementById('OVVBeacon_' + index + '_' + id).isVisible())
-			{
-				visible += 1;
-			}
-		}
+        for (var index = 1; index <= 5; index++) {
+            if (document.getElementById('OVVBeacon_' + index + '_' + id).isVisible()) {
+                visible += 1;
+            }
+        }
 
-		return visible >= 3;
-	}
+        return visible >= 3;
+    }
 
-	function beaconStarted(index)
-	{
-		beaconsStarted[index] = true;
-	}
+    function beaconStarted(index) {
+        beaconsStarted[index] = true;
+    }
 
-	addSWFs('BEACON_SWF_URL'); // 'BEACON_SWF_URL' is string substituted from AS
+    addSWFs('BEACON_SWF_URL'); // 'BEACON_SWF_URL' is string substituted from AS
 
-	return {
-		isPlayerVisible: isPlayerVisible,
-		beaconStarted: beaconStarted
-	};
+    return {
+        isPlayerVisible: isPlayerVisible,
+        beaconStarted: beaconStarted
+    };
 
 })();
