50a4024e2b213c47a8dce6f3032eb96f82ae3e1a
Brandon Dement <bdement@brightroll.com>
2014-06-25 17:03:54 -0700

    added additional user agent check for IE11

--- js/OVVAsset.js
+++ js/OVVAsset.js
@@ -62,7 +62,7 @@ function OVV() {
         version: (userAgent.match(/.+(?:rv|it|ra|ie)[\/: ]([\d.]+)/) || [])[1],
         safari: /webkit/.test(userAgent),
         opera: /opera/.test(userAgent),
-        msie: /msie/.test(userAgent) && !/opera/.test(userAgent),
+        msie: (/msie/.test(userAgent) || /Trident\/7\./.test(userAgent)) && !/opera/.test(userAgent),
         mozilla: /mozilla/.test(userAgent) && !/(compatible|webkit)/.test(userAgent)
     };
 
